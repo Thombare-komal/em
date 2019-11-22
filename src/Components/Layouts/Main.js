7bba84a9996815343ce8d6ea82978785f7eac0a9
akshay dighe <digheakshay2014@gmail.com>
2019-11-22 19:21:21 +0530

    login component

--- src/Components/Layouts/Main.js
+++ src/Components/Layouts/Main.js
@@ -1,12 +1,14 @@
 import React, { Component, useState } from 'react';
-import Dashboard from '../Dashbord'
+import Dashboard from '../Dashbord';
+import Login from '../login/Login';
 
 class Main extends Component {
 
     render() {
         return (
             <div>
-              <Dashboard />
+              {/* <Dashboard /> */}
+              <Login></Login>
             </div>
         )
     }
--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
+import banner from '../../assets/banner.jpg';
+import './Login.css';
 
 class Login extends Component {
 
-    render(){
+    render() {
         return (
             <div>
-                <h1>Login</h1>
+                <Row>
+                    <Col>
+                        <img src={banner} />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="username" className="text-center" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="password" className="text-center"  />
+                            </Form.Group>
+                            <Button variant="primary"  block type="submit">
+                                Login
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
             </div>
         )
     }
