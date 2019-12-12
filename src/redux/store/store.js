0878119a45d656b9e1151d3dcb916de2e9295ced
AkshayDigheGithub <digheakshay2014@gmail.com>
2019-12-12 10:22:17 +0530

    logger and devkit added

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/reducers';
+import { logger } from 'redux-logger';
 
-const store = createStore(rootReducer);
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const middleware = [logger];
+
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware), composeEnhancer));
 
 export default store;
